f59538d2da8375e8fad61a113afa12ae1a68e088
barbosdiego <diego.gutemberg@hotmail.com>
2022-05-17 11:05:43 -0300

    js url params

--- js/main.js
+++ js/main.js
@@ -5,8 +5,19 @@ function activeMenu(link) {
   const href = link.href;
   if (url.includes(href)) {
     link.classList.add('active');
-    console.log(href)
+    console.log(href);
   }
 }
 
 links.forEach(activeMenu);
+
+const parans = new URLSearchParams(location.search);
+
+function activeProduct(param) {
+  const element = document.getElementById(param);
+  if (element) {
+    element.checked = true;
+  }
+}
+
+parans.forEach(activeProduct);
