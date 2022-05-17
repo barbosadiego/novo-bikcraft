b05d915a5165701124cf3d06279f67b7658db13a
barbosdiego <diego.gutemberg@hotmail.com>
2022-05-17 14:25:20 -0300

    js galeria

--- js/main.js
+++ js/main.js
@@ -33,10 +33,23 @@ function ativarPerguta(event) {
   const resposta = document.getElementById(controls);
 
   resposta.classList.toggle('active');
-  const ativa = resposta.classList.contains('active')
-  pergunta.setAttribute('aria-expanded', ativa)
+  const ativa = resposta.classList.contains('active');
+  pergunta.setAttribute('aria-expanded', ativa);
 }
 
 perguntas.forEach((pergunta) =>
   pergunta.addEventListener('click', ativarPerguta),
 );
+
+//Galeria de bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function eventosGaleria(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width:1000px)').matches;
+  if (media) galeriaContainer.prepend(img);
+}
+
+galeria.forEach((img) => img.addEventListener('click', eventosGaleria));
